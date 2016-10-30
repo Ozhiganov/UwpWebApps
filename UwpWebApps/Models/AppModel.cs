4b96046332b08a55055fc2ec04a15a1e7abfaf57
Alex Titarenko <alex.titarenko@live.com>
2016-10-30 14:30:49 +0200

    add appmodel validation

--- UwpWebApps/Models/AppModel.cs
+++ UwpWebApps/Models/AppModel.cs
@@ -1,10 +1,21 @@
-﻿namespace UwpWebApps.Models
+﻿using Prism.Validation;
+using System.ComponentModel.DataAnnotations;
+using UwpWebApps.Validation;
+
+
+namespace UwpWebApps.Models
 {
-    public class AppModel
+    public class AppModel : ValidatableBindableBase
     {
         #region Fields
 
         private static readonly string WebAppTilePrefix = "WebApp-";
+
+        private string _name;
+        private string _baseUrl;
+        private string _accentColor;
+        private string _iconPath;
+        private string _domContentLoadedScript;
 
         #endregion
 
@@ -20,15 +31,49 @@
             }
         }
 
-        public string Name { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
 
-        public string BaseUrl { get; set; }
+        [Required]
+        [UrlValidator(ErrorMessage = "The field BaseUrl is invalid Url.")]
+        public string BaseUrl
+        {
+            get { return _baseUrl; }
+            set { SetProperty(ref _baseUrl, value); }
+        }
 
-        public string AccentColor { get; set; }
+        public string AccentColor
+        {
+            get { return _accentColor; }
+            set { SetProperty(ref _accentColor, value); }
+        }
 
-        public string IconPath { get; set; }
+        [Required]
+        public string IconPath
+        {
+            get { return _iconPath; }
+            set { SetProperty(ref _iconPath, value); }
+        }
 
-        public string DOMContentLoadedScript { get; set; }
+        [StringLength(2000)]
+        public string DOMContentLoadedScript
+        {
+            get { return _domContentLoadedScript; }
+            set { SetProperty(ref _domContentLoadedScript, value); }
+        }
+
+        public bool IsValid
+        {
+            get
+            {
+                return Errors.Errors.Count == 0;
+            }
+        }
 
         #endregion
 
@@ -37,6 +82,9 @@
         public AppModel()
         {
             AccentColor = "DodgerBlue";
+
+            ValidateProperties();
+            ErrorsChanged += AppModel_ErrorsChanged;
         }
 
         #endregion
@@ -68,6 +116,11 @@
             BaseUrl = from.BaseUrl;
             DOMContentLoadedScript = from.DOMContentLoadedScript;
             IconPath = from.IconPath;
+        }
+
+        private void AppModel_ErrorsChanged(object sender, System.ComponentModel.DataErrorsChangedEventArgs e)
+        {
+            OnPropertyChanged(nameof(IsValid));
         }
 
         #endregion
---
+++ UwpWebApps/Validation/UrlValidator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace UwpWebApps.Validation
+{
+    public class UrlValidatorAttribute : DataTypeAttribute
+    {
+        private static Regex Regex;
+
+        static UrlValidatorAttribute()
+        {
+            Regex = CreateRegEx();
+        }
+
+        public UrlValidatorAttribute() : base(DataType.Url)
+        {
+        }
+
+
+        private static Regex CreateRegEx()
+        {
+            TimeSpan timeSpan = TimeSpan.FromSeconds(2);
+            try
+            {
+                Regex regex = new Regex("^(https?|ftp):\\/\\/(((([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:)*@)?(((\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5]))|((([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.)+(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.?)(:\\d*)?)(\\/((([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:|@)+(\\/(([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:|@)*)*)?)?(\\?((([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:|@)|[\\uE000-\\uF8FF]|\\/|\\?)*)?(\\#((([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:|@)|\\/|\\?)*)?$", RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture | RegexOptions.Compiled, timeSpan);
+                return regex;
+            }
+            catch
+            {
+                return new Regex("^(https?|ftp):\\/\\/(((([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:)*@)?(((\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5]))|((([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.)+(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.?)(:\\d*)?)(\\/((([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:|@)+(\\/(([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:|@)*)*)?)?(\\?((([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:|@)|[\\uE000-\\uF8FF]|\\/|\\?)*)?(\\#((([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(%[\\da-f]{2})|[!\\$&'\\(\\)\\*\\+,;=]|:|@)|\\/|\\?)*)?$", RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture | RegexOptions.Compiled);
+            }
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+            string str = value as string;
+
+            if (str == null)
+            {
+                return false;
+            }
+            return Regex.Match(str.Trim()).Length > 0;
+        }
+    }
+}
